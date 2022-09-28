b2b6ce685dc559d154bf3c82c00a6560b69a746c
mehmedGIT <mehmed.n.mustafa@gmail.com>
2022-09-28 14:19:36 +0200

    Support dockerized scripts

--- converter.py
+++ converter.py
@@ -22,6 +22,7 @@ OUT_DIR = 'output_dir'
 # input/output dirs placeholders
 IN_DIR_PH = f'${BRACKETS[0]}{IN_DIR}{BRACKETS[1]}'
 OUT_DIR_PH = f'${BRACKETS[0]}{OUT_DIR}{BRACKETS[1]}'
+DOCKER_PREFIX = f'${BRACKETS[0]}params.docker_command{BRACKETS[1]}'
 
 class Converter:
     def __init__(self):
@@ -32,7 +33,7 @@ class Converter:
         # to hold separate tokens of that line
         self.nf_lines = []
 
-    def convert_OtoN(self, input_path, output_path):
+    def convert_OtoN(self, input_path, output_path, dockerized=False):
         validator = OCRD_Validator()
         validator.validate_ocrd_file(input_path)
         ocrd_commands = validator.extract_ocrd_commands()
@@ -41,38 +42,55 @@ class Converter:
         self.nf_lines = []
 
         # Rule 1: Create the default beginning of a Nextflow script
-        self.__create_default_nextflow_beginning()
+        self.__create_default_nextflow_beginning(dockerized=dockerized)
 
         # Rule 2: For each ocrd command (on each line) a separate Nextflow process is created
-        nf_processes = self.__create_nextflow_processes(ocrd_commands)
+        nf_processes = self.__create_nextflow_processes(ocrd_commands, dockerized=dockerized)
 
         # Rule 3: Create the main workflow
         self.__create_main_workflow(nf_processes)
 
         self.__write_to_nextflow_file(output_path)
-	
-    def __create_default_nextflow_beginning(self):
+    
+    def __create_default_nextflow_beginning(self, dockerized=False):
         self.nf_lines.append(DSL2)
         self.nf_lines.append('')
 
-        # The defaul pipeline parameters
-        # Set venv_path, _workspace_path, and _mets_path appropriately
+        # I do not really like what is happening here
+        # TODO: Maybe a config file is not the right approach or should be modified.
+        # The default pipeline parameters
         with open('config.toml', mode='rb') as toml_f:
             config = tomli.load(toml_f)
-            venv_path = config['venv_path']
-            params_venv = f'params.venv = {QM}{venv_path}{QM}'
             workspace_path = config['workspace_path']
-            params_workspace = f'params.workspace = {QM}{workspace_path}{QM}'
             mets_path = config['mets_path']
+            params_workspace = f'params.workspace = {QM}{workspace_path}{QM}'
             params_mets = f'params.mets = {QM}{mets_path}{QM}'
+            self.nf_lines.append(params_workspace)
+            self.nf_lines.append(params_mets)
 
-        self.nf_lines.append(params_venv)
-        self.nf_lines.append(params_workspace)
-        self.nf_lines.append(params_mets)
+            if dockerized:
+                docker_pwd = config['docker_pwd']
+                docker_volume = config['docker_volume']
+                docker_image = config['docker_image']
+                docker_command = config['docker_command']
+                params_docker_pwd = f'params.docker_pwd = {QM}{docker_pwd}{QM}'
+                params_docker_volume = f'params.docker_volume = {QM}{docker_volume}{QM}'
+                params_docker_image = f'params.docker_image = {QM}{docker_image}{QM}'
+                params_docker_command = f'params.docker_command = {QM}{docker_command}{QM}'
+                self.nf_lines.append(params_docker_pwd)
+                self.nf_lines.append(params_docker_volume)
+                self.nf_lines.append(params_docker_image)
+                self.nf_lines.append(params_docker_command)
+            else:
+                venv_path = config['venv_path']
+                params_venv = f'params.venv = {QM}{venv_path}{QM}'
+                self.nf_lines.append(params_venv)
+
         self.nf_lines.append('')
 
-    def __create_nextflow_processes(self, ocrd_commands):
+    def __create_nextflow_processes(self, ocrd_commands, dockerized=False):
         nf_processes = []
+
         for oc in ocrd_commands:
             nf_process_name = self._extract_nf_process_name(oc[0])
 
@@ -102,9 +120,12 @@ class Converter:
             self.nf_lines.append('')
             self.nf_lines.append(f'{TAB}script:')
             self.nf_lines.append(f'{TAB}"""')
-            self.nf_lines.append(f'{TAB}source "${BRACKETS[0]}params.venv{BRACKETS[1]}"')
-            self.nf_lines.append(f'{TAB}{oc_bash_str}')
-            self.nf_lines.append(f'{TAB}deactivate')
+            if dockerized:
+                self.nf_lines.append(f'{TAB}{DOCKER_PREFIX} {oc_bash_str}')
+            else:
+                self.nf_lines.append(f'{TAB}source "${BRACKETS[0]}params.venv{BRACKETS[1]}"')
+                self.nf_lines.append(f'{TAB}{oc_bash_str}')
+                self.nf_lines.append(f'{TAB}deactivate')
             self.nf_lines.append(f'{TAB}"""')
             self.nf_lines.append(BRACKETS[1])
             self.nf_lines.append('')
@@ -112,7 +133,7 @@ class Converter:
             nf_processes.append([nf_process_name, oc_in, oc_out])
 
         return nf_processes
-	
+    
     def __create_main_workflow(self, nf_processes):
         self.nf_lines.append(f'workflow {BRACKETS[0]}')
         self.nf_lines.append(f'{TAB}main:')
@@ -124,7 +145,7 @@ class Converter:
                 self.nf_lines.append(f'{TAB}{TAB}{nfp[0]}(params.mets, {previous_nfp}.out, {nfp[2]})')
             previous_nfp = nfp[0]
         self.nf_lines.append(f'{BRACKETS[1]}')
-	
+    
     def __write_to_nextflow_file(self, output_path):
         # self.__print_nextflow_tokens()
         # Write Nextflow line tokens to an output file
@@ -164,7 +185,9 @@ def main(argv):
     converter = Converter()
     print(f"OtoN> In: {input_path}")
     print(f"OtoN> Out: {output_path}")
-    converter.convert_OtoN(input_path, output_path)
+    # Change dockerized to True to get the dockerized version of the Nextflow script
+    # TODO: This will be supported by the cli
+    converter.convert_OtoN(input_path, output_path, dockerized=False)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
