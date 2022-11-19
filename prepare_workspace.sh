46cd4efb1f77a6ba7ba02dfb2506f5516b3b181f
mehmedGIT <mehmed.n.mustafa@gmail.com>
2022-11-19 21:05:25 +0100

    Simplify prepare workspace and clean scripts

--- prepare_workspace.sh
+++ prepare_workspace.sh
@@ -3,44 +3,21 @@
 SCRIPT_NAME=$(basename $0)
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-ZIP_LINK="https://ocr-d-repo.scc.kit.edu/api/v1/dataresources/b22282d5-a206-4def-9021-7302199f7326/data/mangoldt_unternehmergewinn_1855.ocrd.zip"
-ZIP_PATH="$SCRIPT_DIR/mangoldt_unternehmergewinn_1855.ocrd.zip"
+METS_ID="PPN631277528.mets.xml"
+METS_LINK="https://content.staatsbibliothek-berlin.de/dc/$METS_ID"
+WS_PATH="$SCRIPT_DIR/ocrd-workspace"
+METS_PATH="$WS_PATH/mets.xml"
+FILE_GROUP="DEFAULT"
 
+rm -rf $WS_PATH
+mkdir $WS_PATH
+wget $METS_LINK -P $WS_PATH
+mv $WS_PATH/*mets.xml $WS_PATH/mets.xml
 
-if [ ! -e $ZIP_PATH ]; then
-   wget $ZIP_LINK -P $SCRIPT_DIR
-fi
+# ocrd software must be available, 
+# otherwise the following lines will not execute
+cd $WS_PATH
+ocrd workspace find --file-grp $FILE_GROUP --download
 
-unzip $ZIP_PATH -d $SCRIPT_DIR/temp
+chmod 777 $WS_PATH
 
-# Copy the OCR-D-IMG folder inside an ocrd-workspace folder
-mkdir -p $SCRIPT_DIR/ocrd-workspace/OCR-D-IMG
-cp -r $SCRIPT_DIR/temp/data/OCR-D-IMG/* $SCRIPT_DIR/ocrd-workspace/OCR-D-IMG
-
-# Give all permisions
-chmod -Rf 777 $SCRIPT_DIR/ocrd-workspace/OCR-D-IMG
-
-rm -rf $SCRIPT_DIR/temp
-
-# ocrd software must be installed beforehand, otherwise the following lines will not execute
-# for the sake of script simplicity checks whether the software is installed or not are not done here!
-
-# Based on https://ocr-d.de/en/user_guide#non-existing-mets
-#ocrd workspace -d $SCRIPT_DIR/ocrd-workspace init
-#ocrd workspace -d $SCRIPT_DIR/ocrd-workspace set-id "test_for_workflows"
-
-#FILEGRP="OCR-D-IMG"
-#EXT=".jpg"  # the actual extension of the image files
-#MEDIATYPE="image/jpeg"  # the actual media type of the image files
-
-#cd $SCRIPT_DIR/ocrd-workspace
-
-#for i in $SCRIPT_DIR/ocrd-workspace/OCR-D-IMG/*$EXT; do
-#  base=`basename ${i} $EXT`; # base holds OCR-D-IMG_*
-#  base_num=${base##*_}; # base holds *
-#  ocrd workspace add -G $FILEGRP -i ${FILEGRP}_${base_num} -g P_${base_num} -m $MEDIATYPE ${i};
-#done
-
-# Alternatively, executing the following script does the job! 
-cd $SCRIPT_DIR/ocrd-workspace
-/bin/bash $SCRIPT_DIR/ocrd-import.sh
